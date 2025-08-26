e1f109bffda0e7b12cbcc8194e46dbfd57a00dd1
Petar Torre <petar.torre@intel.com>
2025-08-26 10:01:54 +0000

    multi

--- build/build.sh
+++ build/build.sh
@@ -8,5 +8,5 @@ set -e
 source config
 
 for n in "${COLLECTOR_IMAGENAME}" "${AGGREGATOR_IMAGENAME}"; do
-    docker build -t "${n}-${HOSTTYPE}" -f "Dockerfile-${n}" .
+    docker build --build-arg "hosttype=${HOSTTYPE}" -t "${n}-${HOSTTYPE}" -f "Dockerfile-${n}" .
 done
