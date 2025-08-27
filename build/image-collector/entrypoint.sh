3626c413b80c84988c1a0f618c34a5e2296094be
Petar Torre <petar.torre@intel.com>
2025-08-27 09:45:15 +0000

    by with

--- build/image-collector/entrypoint.sh
+++ build/image-collector/entrypoint.sh
@@ -22,7 +22,7 @@ mkdir results
 while [[ 1 ]]; do
     perfspect report --log-stdout --noupdate --output results --format json,txt 1>> /dev/null 2>> /dev/null
     for f in "json" "txt"; do
-        sed -i "s/${HOSTNAME}/${NODE_NAME}/g" results/*.${f}
+        sed -i "s/${HOSTNAME}/${NODE_NAME}/g;s/by Intel /with Intel PerfSpect /g" results/*.${f}
         if [[ ! -z "${AGGREGATOR_ENDPOINT}" ]]; then
             curl -X POST "${AGGREGATOR_ENDPOINT}" \
                 -F "filename=${NODE_NAME}.${f}" \
