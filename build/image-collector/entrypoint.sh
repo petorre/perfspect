238896a028aff62b58d60703f6c29f357086e63f
Petar Torre <petar.torre@intel.com>
2025-08-28 08:40:34 +0000

    categories

--- build/image-collector/entrypoint.sh
+++ build/image-collector/entrypoint.sh
@@ -9,6 +9,12 @@ if [[ -z "${SLEEP}" ]]; then
     SLEEP=60
 fi
 
+if [[ -z "${CATEGORIES}" ]]; then
+    CATEGORIES="--all"
+else
+    CATEGORIES=$( echo "${CATEGORIES}" | sed "s/\b\w\+/--&/g" )
+fi
+
 HOSTNAME=$( hostname )
 if [[ -z "${NODE_NAME}" ]]; then
     NODE_NAME="${HOSTNAME}"
@@ -20,7 +26,7 @@ cd /root/perfspect
 mkdir results
 
 while [[ 1 ]]; do
-    perfspect report --log-stdout --noupdate --output results --format json,txt 1>> /dev/null 2>> /dev/null
+    perfspect report "${CATEGORIES}" --log-stdout --noupdate --output results --format json,txt 1>> /dev/null 2>> /dev/null
     for f in "json" "txt"; do
         sed -i "s/${HOSTNAME}/${NODE_NAME}/g;s/by Intel /with Intel PerfSpect /g" results/*.${f}
         if [[ ! -z "${AGGREGATOR_ENDPOINT}" ]]; then
